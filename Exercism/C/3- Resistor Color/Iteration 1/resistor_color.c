5613b10f6768e7cb6340a366440038fe7e33a953
Mina Karam <minakaram.me@gmail.com>
2022-05-15 17:29:37 +0200

    Add 3- Resistor Color, Iteration 1

---
+++ Exercism/C/3- Resistor Color/Iteration 1/resistor_color.c
@@ -0,0 +1,13 @@
+#include "resistor_color.h"
+
+static resistor_band_t bands[] = {BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE};
+
+resistor_band_t color_code(resistor_band_t code)
+{
+    return bands[code];
+}
+
+resistor_band_t* colors(void)
+{
+    return bands;
+}
